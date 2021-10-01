7e2ba7d1ede0aff698752d28d1c4b63de0565033
unknown <albert.khurshudyan96@gmail.com>
2021-10-02 00:57:41 +0300

    Updated Problems

--- MS SQL/Subqueries and Joins/1-17 JOINS Exercises.sql
+++ MS SQL/Subqueries and Joins/1-17 JOINS Exercises.sql
@@ -3,8 +3,27 @@ FROM Employees
 JOIN Addresses ON Employees.AddressID = Addresses.AddressID
 ORDER BY Employees.AddressID
 
-
-SELECT TOP(50) FirstName, LastName, Towns.Name AS Town, 
+SELECT TOP(50) Employees.FirstName, Employees.LastName, Towns.Name AS Town, Addresses.AddressText
 FROM Employees
-Join Towns ON Employees.EmployeeID = Towns.TownID
-ORDER BY FirstName
+JOIN Addresses ON Employees.AddressID = Addresses.AddressID
+JOIN Towns ON Addresses.TownID = Towns.TownID
+ORDER BY Employees.FirstName, Employees.LastName;
+
+SELECT Employees.EmployeeID, Employees.FirstName, Employees.LastName, Departments.Name AS DepartmentName
+FROM Employees
+JOIN Departments ON Employees.DepartmentID = Departments.DepartmentID
+WHERE Employees.JobTitle LIKE '%Sales%'
+ORDER BY Employees.EmployeeID
+
+SELECT TOP(5) Employees.EmployeeID, Employees.FirstName, Employees.Salary, Departments.Name AS DepartmentName
+FROM Employees
+JOIN Departments ON Employees.DepartmentID = Departments.DepartmentID
+WHERE Employees.Salary > 15000
+ORDER BY Departments.DepartmentID
+
+SELECT TOP(3) Employees.EmployeeID, Employees.FirstName
+FROM Employees
+LEFT JOIN EmployeesProjects ON Employees.EmployeeID = EmployeesProjects.EmployeeID
+LEFT JOIN  Projects ON EmployeesProjects.EmployeeID = Projects.ProjectID
+WHERE EmployeesProjects.ProjectID IS NULL
+ORDER BY Employees.EmployeeID
