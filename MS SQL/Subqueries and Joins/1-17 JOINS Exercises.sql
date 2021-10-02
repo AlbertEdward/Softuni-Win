2e306c8267195611235df4b4216ac2b700c8952e
unknown <albert.khurshudyan96@gmail.com>
2021-10-02 22:04:58 +0300

    Exercises

--- MS SQL/Subqueries and Joins/1-17 JOINS Exercises.sql
+++ MS SQL/Subqueries and Joins/1-17 JOINS Exercises.sql
@@ -27,3 +27,27 @@ LEFT JOIN EmployeesProjects ON Employees.EmployeeID = EmployeesProjects.Employee
 LEFT JOIN  Projects ON EmployeesProjects.EmployeeID = Projects.ProjectID
 WHERE EmployeesProjects.ProjectID IS NULL
 ORDER BY Employees.EmployeeID
+
+SELECT Employees.FirstName, Employees.LastName, Employees.HireDate, Departments.Name AS DeptName
+FROM Employees
+JOIN Departments ON Departments.DepartmentID = Employees.DepartmentID
+WHERE HireDate > 1999-01-01 AND (Departments.Name = 'Sales' OR Departments.Name = 'Finance')
+ORDER BY HireDate
+
+SELECT TOP(5) Employees.EmployeeID, Employees.FirstName, Projects.Name AS ProjectName
+FROM Employees
+JOIN EmployeesProjects ON Employees.EmployeeID = EmployeesProjects.EmployeeID
+JOIN Projects ON Projects.ProjectID = EmployeesProjects.ProjectID
+WHERE Projects.StartDate > 2002-08-13 AND Projects.EndDate IS NULL
+ORDER BY EmployeeID
+
+SELECT Employees.EmployeeID, Employees.FirstName, Projects.Name AS ProjectName
+FROM Employees
+JOIN EmployeesProjects ON Employees.EmployeeID = EmployeesProjects.EmployeeID
+LEFT JOIN Projects ON Projects.ProjectID = EmployeesProjects.ProjectID AND StartDate < '2005-01-01'
+WHERE EmployeesProjects.EmployeeID = 24
+
+SELECT Employees.EmployeeID, Employees.FirstName, Employees.ManagerID, Departments.Name AS ManagerName
+
+
+SELECT * FROM Departments
