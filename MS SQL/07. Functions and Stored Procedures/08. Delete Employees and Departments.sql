5243571ade96647a03eb152d625bc8bb59417406
unknown <albert.khurshudyan96@gmail.com>
2021-10-12 23:34:16 +0300

    Updated

--- MS SQL/07. Functions and Stored Procedures/08. Delete Employees and Departments.sql
+++ MS SQL/07. Functions and Stored Procedures/08. Delete Employees and Departments.sql
@@ -1,10 +1,31 @@
-CREATE PROC usp_DeleteEmployeesFromDepartment (@departmentId int)
+SELECT EmployeeID
+FROM Employees
+WHERE DepartmentID = 4
+
+CREATE PROC usp_DeleteEmployeesFromDepartment @departmentId int
 AS
 BEGIN
-	DELETE FROM Employees
-	WHERE DepartmentID = @departmentId
+	DELETE FROM EmployeesProjects
+	WHERE EmployeeID IN (
+						SELECT EmployeeID
+						FROM Employees
+						WHERE DepartmentID = @departmentId
+						)
 
+	UPDATE Employees
+	SET ManagerID = NULL
+	WHERE ManagerID IN (
+						SELECT EmployeeID
+						FROM Employees
+						WHERE DepartmentID = @departmentId
+						)
 	
-END
+	DELETE FROM Employees
+	WHERE 
+	IN (
+						SELECT EmployeeID
+						FROM Employees
+						WHERE DepartmentID = @departmentId
+						)
 
-ALTER TABLE Employees ALTER COLUMN ManagerId int NULL
+END
