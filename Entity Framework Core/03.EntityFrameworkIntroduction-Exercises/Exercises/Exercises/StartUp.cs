2868e72e597fb11a800b77c7d5995c51c7bece97
unknown <albert.khurshudyan96@gmail.com>
2021-11-09 00:29:19 +0200

    Next Exercise

--- Entity Framework Core/03.EntityFrameworkIntroduction-Exercises/Exercises/Exercises/StartUp.cs
+++ Entity Framework Core/03.EntityFrameworkIntroduction-Exercises/Exercises/Exercises/StartUp.cs
@@ -1,12 +1,36 @@
-﻿using System;
-
-namespace SoftUni
+﻿namespace SoftUni
 {
-    class StartUp
+    using SoftUni.Data;
+    using SoftUni.Models;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SoftUniContext sk = new SoftUniContext();
+            string context = GetEmployeesFullInformation(sk);
+            Console.WriteLine(context);
+
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Employee[] employees = context
+                .Employees
+                .OrderBy(e => e.EmployeeId)
+                .ToArray();
+
+            foreach (Employee e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
+    
 }
