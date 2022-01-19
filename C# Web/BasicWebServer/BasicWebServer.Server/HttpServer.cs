85ec395c634b09f08f7a4f358569d5e820359e34
Albert Khurshudyan <albert.khurshudyan96@gmail.com>
2022-01-19 19:23:13 +0200

    update

---
+++ C# Web/BasicWebServer/BasicWebServer.Server/HttpServer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Server
+{
+    internal class HttpServer
+    {
+        private readonly IPAddress iPAddress;
+        private readonly int port;
+        private readonly TcpListener serverListener;
+
+        public HttpServer(string ipAddress, int port)
+        {
+            this.iPAddress = IPAddress.Parse(ipAddress);
+            this.port = port;
+
+            this.serverListener = new TcpListener(this.ipAddress,port);
+        }
+    }
+}
