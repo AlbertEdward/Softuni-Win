c69edee8800d349ac1fc4b90b9fcb11348499b04
Albert Khurshudyan <albert.khurshudyan96@gmail.com>
2022-01-20 23:00:16 +0200

    updated web server

--- C# Web/BasicWebServer/BasicWebServer.Server/HttpServer.cs
+++ C# Web/BasicWebServer/BasicWebServer.Server/HttpServer.cs
@@ -8,18 +8,82 @@ using System.Threading.Tasks;
 
 namespace BasicWebServer.Server
 {
-    internal class HttpServer
+    public class HttpServer
     {
-        private readonly IPAddress iPAddress;
+        private readonly IPAddress ipAddress;
         private readonly int port;
         private readonly TcpListener serverListener;
 
         public HttpServer(string ipAddress, int port)
         {
-            this.iPAddress = IPAddress.Parse(ipAddress);
+            this.ipAddress = IPAddress.Parse(ipAddress);
             this.port = port;
 
-            this.serverListener = new TcpListener(this.ipAddress,port);
+            this.serverListener = new TcpListener(this.ipAddress, port);
+
+        }
+
+        public void Start()
+        {
+            this.serverListener.Start();
+
+            Console.WriteLine($"Server started on port {port}...");
+            Console.WriteLine("Listening for requests...");
+
+            while (true)
+            {
+                var connection = serverListener.AcceptTcpClient();
+                var networkStream = connection.GetStream();
+
+                var requestText = this.ReadRequest(networkStream);
+
+                Console.WriteLine(requestText);
+
+                WriteResponse(networkStream, "Hello from the server!");
+
+                connection.Close();
+            }
+        }
+
+        private void WriteResponse(NetworkStream networkStream, string message)
+        {
+            var contentLength = Encoding.UTF8.GetByteCount(message);
+
+            var response = $@"HTTP/1.1 200 OK
+Content-Type: text/plain; charset=UTF-8
+Content-Length: {contentLength}
+
+{message}";
+
+            var responseByte = Encoding.UTF8.GetBytes(response);
+
+            networkStream.Write(responseByte);
+        }
+
+        private string ReadRequest(NetworkStream networkStream)
+        {
+            var bufferLength = 1024;
+            var buffer = new byte[bufferLength];
+
+            var totalBytes = 0;
+
+            var requestBuilder = new StringBuilder();
+
+            do
+            {
+                var bytesRead = networkStream.Read(buffer, 0, bufferLength);
+
+                totalBytes += bytesRead;
+                if (totalBytes > 10 * 1024)
+                {
+                    throw new InvalidOperationException("Request is too large.");
+                }
+
+                requestBuilder.Append(Encoding.UTF8.GetString(buffer, 0, bytesRead));
+
+            } while (networkStream.DataAvailable);
+
+            return requestBuilder.ToString();
         }
     }
 }
--- C# Web/BasicWebServer/BasicWebServer/BasicWebServer.cs
+++ C# Web/BasicWebServer/BasicWebServer/BasicWebServer.cs
@@ -1,4 +1,47 @@
-﻿using BasicWebServer;
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
-var server = new HttpServer("127.0.0.1", 8080);
-server.Start();
+namespace BasicWebServer
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var ipAddress = IPAddress.Parse("127.0.0.1");
+            var port = 8080;
+
+            var serverListener = new TcpListener(ipAddress, port);
+            serverListener.Start();
+
+            Console.WriteLine($"Server started on port {port}");
+            Console.WriteLine("Listening for request...");
+
+            while (true)
+            {
+                var connection = serverListener.AcceptTcpClient();
+
+                var networkStream = connection.GetStream();
+
+                var content = "Hello from the server!";
+                var contentLength = Encoding.UTF8.GetByteCount(content);
+
+                var response = $@"HTTP/1.1 200 OK
+Content-Type: text/plain; charset=UTF-8
+Content-Length: {contentLength}
+
+{content}";
+
+                var responseByte = Encoding.UTF8.GetBytes(response);
+
+                networkStream.Write(responseByte);
+
+                connection.Close();
+            }
+        }
+    }
+}
+
+
+
+
